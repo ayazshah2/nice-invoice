6f5fb6879b8aadfb6d30309ffa1c0066f334cbb1
bakhtawarshah <cartlicms@gmail.com>
2020-08-29 18:15:31 +0400

    invoice script

--- app.js
+++ app.js
@@ -1,5 +1,191 @@
-const niceInvoice = params => {
-    return params;
+const fs = require("fs");
+const PDFDocument = require("pdfkit");
+
+function niceInvoice(invoice, path) {
+  let doc = new PDFDocument({ size: "A4", margin: 50 });
+
+  header(doc);
+  customerInformation(doc, invoice);
+  invoiceTable(doc, invoice);
+  footer(doc);
+
+  doc.end();
+  doc.pipe(fs.createWriteStream(path));
+}
+
+function header(doc) {
+  doc
+    .image("logo.png", 50, 45, { width: 50 })
+    .fillColor("#444444")
+    .fontSize(20)
+    .text("XYZ Inc.", 110, 57)
+    .fontSize(10)
+    .text("XYZ Inc.", 200, 50, { align: "right" })
+    .text("123 Main Street", 200, 65, { align: "right" })
+    .text("Dubai, UAE, 12345", 200, 80, { align: "right" })
+    .moveDown();
+}
+
+function customerInformation(doc, invoice) {
+  doc
+    .fillColor("#444444")
+    .fontSize(20)
+    .text("Invoice", 50, 160);
+
+  generateHr(doc, 185);
+
+  const customerInformationTop = 200;
+
+  doc
+    .fontSize(10)
+    .text("Invoice Number:", 50, customerInformationTop)
+    .font("Helvetica-Bold")
+    .text(invoice.invoice_nr, 150, customerInformationTop)
+    .font("Helvetica")
+    .text("Invoice Date:", 50, customerInformationTop + 15)
+    .text(formatDate(new Date()), 150, customerInformationTop + 15)
+    .text("Balance Due:", 50, customerInformationTop + 30)
+    .text(
+      formatCurrency(invoice.subtotal - invoice.paid),
+      150,
+      customerInformationTop + 30
+    )
+
+    .font("Helvetica-Bold")
+    .text(invoice.shipping.name, 300, customerInformationTop)
+    .font("Helvetica")
+    .text(invoice.shipping.address, 300, customerInformationTop + 15)
+    .text(
+      invoice.shipping.city +
+        ", " +
+        invoice.shipping.state +
+        ", " +
+        invoice.shipping.country,
+      300,
+      customerInformationTop + 30
+    )
+    .moveDown();
+
+  generateHr(doc, 252);
+}
+
+function invoiceTable(doc, invoice) {
+  let i;
+  const invoiceTableTop = 330;
+
+  doc.font("Helvetica-Bold");
+  tableRow(
+    doc,
+    invoiceTableTop,
+    "Item",
+    "Description",
+    "Unit Cost",
+    "Quantity",
+    "Line Total"
+  );
+  generateHr(doc, invoiceTableTop + 20);
+  doc.font("Helvetica");
+
+  for (i = 0; i < invoice.items.length; i++) {
+    const item = invoice.items[i];
+    const position = invoiceTableTop + (i + 1) * 30;
+    tableRow(
+      doc,
+      position,
+      item.item,
+      item.description,
+      formatCurrency(item.amount / item.quantity),
+      item.quantity,
+      formatCurrency(item.amount)
+    );
+
+    generateHr(doc, position + 20);
+  }
+
+  const subtotalPosition = invoiceTableTop + (i + 1) * 30;
+  tableRow(
+    doc,
+    subtotalPosition,
+    "",
+    "",
+    "Subtotal",
+    "",
+    formatCurrency(invoice.subtotal)
+  );
+
+  const paidToDatePosition = subtotalPosition + 20;
+  tableRow(
+    doc,
+    paidToDatePosition,
+    "",
+    "",
+    "Paid To Date",
+    "",
+    formatCurrency(invoice.paid)
+  );
+
+  const duePosition = paidToDatePosition + 25;
+  doc.font("Helvetica-Bold");
+  tableRow(
+    doc,
+    duePosition,
+    "",
+    "",
+    "Balance Due",
+    "",
+    formatCurrency(invoice.subtotal - invoice.paid)
+  );
+  doc.font("Helvetica");
+}
+
+function footer(doc) {
+  doc
+    .fontSize(10)
+    .text(
+      "Any footer text - you can add any text here",
+      50,
+      780,
+      { align: "center", width: 500 }
+    );
+}
+
+function tableRow(
+  doc,
+  y,
+  item,
+  description,
+  unitCost,
+  quantity,
+  lineTotal
+) {
+  doc
+    .fontSize(10)
+    .text(item, 50, y)
+    .text(description, 150, y)
+    .text(unitCost, 280, y, { width: 90, align: "right" })
+    .text(quantity, 370, y, { width: 90, align: "right" })
+    .text(lineTotal, 0, y, { align: "right" });
+}
+
+function generateHr(doc, y) {
+  doc
+    .strokeColor("#aaaaaa")
+    .lineWidth(1)
+    .moveTo(50, y)
+    .lineTo(550, y)
+    .stroke();
+}
+
+function formatCurrency(cents) {
+  return "$" + (cents / 100).toFixed(2);
+}
+
+function formatDate(date) {
+  const day = date.getDate();
+  const month = date.getMonth() + 1;
+  const year = date.getFullYear();
+
+  return year + "/" + month + "/" + day;
 }
 
 module.exports = niceInvoice;
