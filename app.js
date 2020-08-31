0731560d1a36103d94bf116a04d612dcca4fe2b1
dev6.0 <60340125+bakhtawarshah@users.noreply.github.com>
2020-08-31 16:54:45 +0400

    fixed tax issue

--- app.js
+++ app.js
@@ -100,7 +100,7 @@ let invoiceTable = (doc, invoice) => {
       formatCurrency(item.price, currencySymbol),
       item.quantity,
       formatCurrency(applyTaxIfAvailable(item.price, item.quantity, item.tax), currencySymbol), 
-      item.tax
+      checkIfTaxAvailable(item.tax)
     );
 
     generateHr(doc, position + 20);
@@ -177,10 +177,25 @@ let formatCurrency = (cents, symbol) => {
 }
 
 let getNumber =  str =>  { 
-  var num = str.replace(/[^0-9]/g, ''); 
+  if(str.length!==0){
+    var num = str.replace(/[^0-9]/g, ''); 
+  }else{
+    var num = 0;
+  }
+  
   return num; 
 }
 
+let checkIfTaxAvailable = tax => {
+  let validatedTax = getNumber(tax);
+  if(Number.isNaN(validatedTax) === false && validatedTax <= 100 && validatedTax > 0){
+    var taxValue = tax;  
+  }else{
+    var taxValue = '---';
+  }
+  
+  return taxValue;
+}
 
 let applyTaxIfAvailable = (price, quantity, tax) => {
   
@@ -190,7 +205,6 @@ let applyTaxIfAvailable = (price, quantity, tax) => {
     let taxValue = '.'+validatedTax;
     var itemPrice = (price * quantity) * (1 + taxValue);  
   }else{
-    console.log('no');
     var itemPrice = (price * quantity) * (1 + taxValue);
   }
   
@@ -208,49 +222,3 @@ let companyAddress = (doc, address) => {
 }
 
 module.exports = niceInvoice;
-
-
-const invoiceDetail = {
-    shipping: {
-      name: "Micheal",
-      address: "1234 Main Street",
-      city: "Dubai",
-      state: "Dubai",
-      country: "UAE",
-      postal_code: 94111
-    },
-    items: [
-      {
-        item: "Chair",
-        description: "Wooden chair",
-        quantity: 3,
-        price: 50.00, 
-        tax: "10%"
-      },
-      {
-        item: "Watch",
-        description: "Wall watch for office",
-        quantity: 1,
-        price: 70.00,
-        tax: "10%"
-      }
-    ],
-    subtotal: 120,
-    total: 120,
-    order_number: 1234222,
-    header:{
-        company_name: "Nice Invoice",
-        company_logo: "logo.png",
-        company_address: "Nice Invoice. 123 William Street 1th Floor New York, NY 123456"
-    },
-    footer:{
-      text: "Any footer text - you can add any text here"
-    },
-    currency_symbol:"$", 
-    date: {
-      billing_date: "08 August 2020",
-      due_date: "10 September 2020",
-    }
-};
-
-niceInvoice(invoiceDetail, 'your-invoice-name.pdf');
